2ed30371efce4f328ff4bf3d8eb0bccd655def36
Favour Ori <favourori@Favours-MacBook-Pro.local>
2019-01-05 04:20:53 -0600

    users routes all set - can create and view all users. Adding Put & delete later

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,48 @@
 let express = require("express");
 let router = express.Router();
+let mongoose = require("mongoose");
 
-//the routes:
+//creating the schema to create users
+
+let userSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+});
+
+let User = mongoose.model("user", userSchema);
+
+//The routes:
+
+//Getting all users
+
 router.get("/", (req, res) => {
-  res.send("Welcome to the Users route..");
+  User.find()
+    .then(users => {
+      res.status(200).send(users);
+    })
+    .catch(err => {
+      res.status(400).send(err.message);
+    });
+});
+
+//Creating a new user
+
+router.post("/", (req, res) => {
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  user
+    .save()
+    .then(newUser => {
+      res.status(200).send(newUser);
+    })
+    .catch(err => {
+      res.status(400).send(err.message);
+    });
 });
 
 //exporting the router
